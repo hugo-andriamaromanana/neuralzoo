e365bcffd2aef4f427471de23a53ff83d5328578
alexandre-assad <alexandre.assad@laplateforme.io>
2025-04-23 16:03:27 +0200

    :fix: better loading of data

--- neuralzoo/loader/load_data.py
+++ neuralzoo/loader/load_data.py
@@ -23,16 +23,17 @@ def load_all_cifar() -> tuple[NDArray, NDArray, NDArray, NDArray]:
     all_dataframes = []
     all_target = []
     for b in range(1,6):
-        f = path.join('../data/', 'data_batch_%d' % (b, ))
+        f = path.join('../data/legacy', 'data_batch_%d' % (b, ))
         dataframe, target = load_cifar_single_batch(f)
         all_dataframes.append(dataframe)
         all_target.append(target)
     dataframes = concatenate(all_dataframes)
     targets = concatenate(all_target)
-    dataframe_test, target_test = load_cifar_single_batch(path.join('../data/', 'test_batch'))
+    dataframe_test, target_test = load_cifar_single_batch(path.join('../data/legacy', 'test_batch'))
     return dataframes, targets, dataframe_test, target_test
 
-def dataframes_from_cifar(num_training=49000, num_validation=1000, num_test=10000) -> tuple[DataFrame, DataFrame, DataFrame, DataFrame]:
+def dataframes_from_cifar(num_training=49000, num_validation=1000, num_test=10000) -> tuple[DataFrame, DataFrame, DataFrame, DataFrame]: 
+    # Leggacy load from pickles
     dataframe_train, target_train, dataframe_test, target_test = load_all_cifar()
 
     mask = range(num_training, num_training + num_validation)
